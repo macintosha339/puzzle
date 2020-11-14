12f69d1176feea4e21454f143bfac179723096c7
AntonMihailov <you@example.com>
2020-11-14 20:52:04 +0300

    feat: implement shuflle cells

--- app.js
+++ app.js
@@ -7,8 +7,9 @@ document.body.appendChild(field);
 const cellSize = 100;
 
 const empty = {
-    top: 0,
-    left: 0
+    value: 16,
+    top: 3,
+    left: 3
 };
 
 const cells = [];
@@ -16,6 +17,13 @@ cells.push(empty);
 
 function move(index) {
     const cell = cells[index];
+    const leftDiff = Math.abs(empty.left - cell.left);
+    const topDiff = Math.abs(empty.top - cell.top);
+
+    if(leftDiff + topDiff > 1) {
+        return;
+    }
+
     cell.element.style.left = `${empty.left * cellSize}px`;
     cell.element.style.top = `${empty.top * cellSize}px`;
 
@@ -25,17 +33,30 @@ function move(index) {
     empty.top = cell.top;
     cell.left = emptyLeft;
     cell.top = emptyTop;
+
+    const isFinished = cells.every(cell => {
+       return cell.value === cell.top * 4 + cell.left;
+    });
+
+    if(isFinished) {
+        alert('You won');
+    }
 }
 
-for(let i = 1; i <= 15; i++) {
+const numbers = [...Array(15).keys()]
+    //.sort(() => Math.random() - 0.5);
+
+for(let i = 0; i <= 14; i++) {
     const cell = document.createElement('div');
+    const value = numbers[i] + 1 ;
     cell.className = 'cell';
-    cell.innerHTML = i;
+    cell.innerHTML = value;
 
     const left = i % 4;
     const top = (i - left) / 4;
 
     cells.push({
+        value: value,
         left: left,
         top: top,
         element: cell
